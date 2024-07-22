d553786e32d0e0f7567fe2d563d6bb6d4f53f6fd
Guilherme <guilhermessmith2014@gmail.com>
2024-07-22 02:51:25 -0300

    tests files cleaned

--- tests.php
+++ tests.php
@@ -14,23 +14,9 @@
             <div class = "text-center">
                 <h1>Testes</h1>
             </div>
-            <form class = "container form-container" method = "POST" action = "tests2.php" enctype="multipart/form-data" >
-                <div class = "form-group text-center">
-                    <label id = "image-label" for="image-input" class = "form-label">
-                        <img id = "label-image" src="media/bootstrap.png" alt="img" width = "50%" height = "50%" style="border: solid 1px black; border-radius: 50%">
-                    </label>
-                    <input type="file" name = "image_src" style = 'display: none' id = "image-input" required />
-                </div>
-                <div class = "form-group" style="text-align:right">
-                    <button type = "submit" class = "btn btn-success">Enviar</button>
-                </div>
-                <div class = "text-center">
-                    <?php var_dump($_SESSION['feedback']['tests']); ?>
-                </div>
-            </form>
-            <div>
-                <img src="http://localhost/ssocial/media/users/13/pfp/eu_669de74c5fcdc.jpeg" alt="img2">
-            </div>
+            <article class = "container" >
+                
+            </article>
         </section>
     </body>
 </html>
